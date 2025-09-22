a88a7736815097daf228481aebedd72c24c1d49a
Oleg Dianov <odidoit@gmail.com>
2025-09-22 21:09:15 +0300

    del shortboard

--- src/pages/top5/tvoisurf39/app.js
+++ src/pages/top5/tvoisurf39/app.js
@@ -111,19 +111,9 @@ document.addEventListener('DOMContentLoaded', function() {
   async function init() {
       const categories = [
           {
-              id: 'shortboard-men',
-              title: 'Короткая доска, Мужчины',
-              link: 'https://odinsy.github.io/topheats-rating/src/pages/rankings/index.html?category=shortboard_men'
-          },
-          {
               id: 'longboard-men',
               title: 'Длинная доска, Мужчины',
               link: 'https://odinsy.github.io/topheats-rating/src/pages/rankings/index.html?category=longboard_men'
-          },
-          {
-              id: 'shortboard-women',
-              title: 'Короткая доска, Женщины',
-              link: 'https://odinsy.github.io/topheats-rating/src/pages/rankings/index.html?category=shortboard_women'
           },
           {
               id: 'longboard-women',
